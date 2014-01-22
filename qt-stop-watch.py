f9e202b20ef8b138eed197188f2e089b223c0446
enkore <public@enkore.de>
2014-01-22 21:42:58 +0100

    Uh, they changed that. Cool.

--- qt-stop-watch.py
+++ qt-stop-watch.py
@@ -6,18 +6,20 @@ from PyQt5 import Qt
 from PyQt5.uic import loadUi
 
 # [ms]
-TICK_TIME = 100
+TICK_TIME = 2**6
 
 class StopWatch(Qt.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = loadUi("gui.ui", self)
-        self.ui.reset.clicked.connect(self.do_reset)
-        self.ui.start.clicked.connect(self.do_start)
+        self.reset.clicked.connect(self.do_reset)
+        self.start.clicked.connect(self.do_start)
 
         self.timer = Qt.QTimer()
         self.timer.setInterval(TICK_TIME)
         self.timer.timeout.connect(self.tick)
+
+        self.do_reset()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Qt.Key_Escape:
@@ -25,31 +27,32 @@ class StopWatch(Qt.QMainWindow):
         else:
             super().keyPressEvent(event)
 
+    def display(self):
+        self.lcd.display("%d:%05.2f" % (self.time // 60, self.time % 60))
+
     @Qt.pyqtSlot()
     def tick(self):
-        time = self.ui.sec.value() + self.ui.min.value() * 60
-        time += TICK_TIME/1000
-        self.ui.sec.display("%0.1f" % (time % 60))
-        self.ui.min.display(time // 60)
+        self.time += TICK_TIME/1000
+        self.display()
 
     @Qt.pyqtSlot()
     def do_start(self):
         self.timer.start()
-        self.ui.start.setText("Pause")
-        self.ui.start.clicked.disconnect()
-        self.ui.start.clicked.connect(self.do_pause)
+        self.start.setText("Pause")
+        self.start.clicked.disconnect()
+        self.start.clicked.connect(self.do_pause)
 
     @Qt.pyqtSlot()
     def do_pause(self):
         self.timer.stop()
-        self.ui.start.setText("Start")
-        self.ui.start.clicked.disconnect()
-        self.ui.start.clicked.connect(self.do_start)
+        self.start.setText("Start")
+        self.start.clicked.disconnect()
+        self.start.clicked.connect(self.do_start)
 
     @Qt.pyqtSlot()
     def do_reset(self):
-        self.ui.sec.display(0)
-        self.ui.min.display(0)
+        self.time = 0
+        self.display()
 
 app = Qt.QApplication(sys.argv)
 
